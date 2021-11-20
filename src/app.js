d003ee3c66e32c28f42c981f14b10f8d08692553
deboracaires <deboracaires@pop-os.localdomain>
2021-11-19 21:11:39 -0300

    rota post signature implementada. rota get signature implementada, quando o usuario faz o get da rota automaticamente o servidor registra as proximas 16 entregas e toda vez que a rota for puxada o servidor puxa todas as entregas e faz a atualizacao das entregas ja feitas

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import { signUp } from './controllers/signUp.js';
 import { signIn } from './controllers/signIn.js';
 import { authorization } from './middlewares/authorization.js';
-import { postSignature } from './controllers/signature.js';
+import { getSignature, postSignature } from './controllers/signature.js';
 
 const app = express();
 
@@ -13,5 +13,7 @@ app.use(express.json());
 app.post('/sign-up', signUp);
 app.post('/sign-in', signIn);
 app.post('/signature', authorization, postSignature);
+
+app.get('/signature', authorization, getSignature);
 
 export default app;
--- src/controllers/signature.js
+++ src/controllers/signature.js
@@ -1,9 +1,11 @@
+/* eslint-disable no-await-in-loop */
 /* eslint-disable consistent-return */
 /* eslint-disable camelcase */
 /* eslint-disable prefer-destructuring */
 /* eslint-disable import/prefer-default-export */
 import dayjs from 'dayjs';
 import connection from '../database/database.js';
+import registerDeliveries from './deliveries.js';
 import { signatureSchema } from '../schemmas/signatureSchema.js';
 
 async function postSignature(req, res) {
@@ -126,6 +128,143 @@ async function postSignature(req, res) {
     res.sendStatus(500);
   }
 }
+
+async function getSignature(req, res) {
+  // eslint-disable-next-line dot-notation
+  const auth = req.headers['authorization'];
+  const token = auth?.replace('Bearer ', '');
+
+  try {
+    const session = await connection.query(`
+        SELECT * FROM sessions WHERE "token" = $1
+    `, [token]);
+
+    const userId = session.rows[0].user_id;
+
+    const signature = await connection.query(`
+        SELECT signatures.id, signatures."signature_date", plan.name, signatures."delivery_plan_id", signatures."signature_date"
+        FROM signatures
+        INNER JOIN plan
+        ON signatures."plan_id"=plan.id
+        WHERE "user_id"=$1
+    `, [userId]);
+
+    if (signature.rowCount === 0) {
+      return res.sendStatus(404);
+    }
+    const {
+      signature_date,
+      name,
+      delivery_plan_id,
+    } = signature.rows[0];
+
+    const plan_name = name;
+
+    const deliveryName = await connection.query(`
+        SELECT * FROM "delivery_plan" WHERE id = $1
+    `, [delivery_plan_id]);
+
+    const delivery_plan = deliveryName.rows[0].name;
+
+    const signatureProducts = await connection.query(`
+        SELECT * FROM "signature_products" WHERE "signature_id"=$1
+    `, [signature.rows[0].id]);
+
+    const products_id = [];
+    for (let i = 0; i < signatureProducts.rowCount; i += 1) {
+      products_id.push(signatureProducts.rows[i].product_id);
+    }
+    const products = [];
+    for (let i = 0; i < products_id.length; i += 1) {
+      // eslint-disable-next-line no-await-in-loop
+      const productName = await connection.query(`
+        SELECT * FROM products WHERE id=$1
+    `, [products_id[i]]);
+
+      products.push(productName.rows[0].name);
+    }
+
+    const deliveries = await connection.query(`
+      SELECT * FROM deliveries WHERE "user_id" = $1 ORDER BY id ASC
+    `, [userId]);
+
+    const nowDate = dayjs().format('MM/DD/YYYY');
+
+    for (let i = 0; i < deliveries.rows.length; i += 1) {
+      if (dayjs(deliveries.rows[i].date).isBefore(nowDate)) {
+        await connection.query(`
+          UPDATE deliveries SET "delivered" = $2 WHERE id = $1
+        `, [deliveries.rows[i].id, true]);
+      }
+    }
+
+    let dates = [];
+    const greaterDates = [];
+    dates = registerDeliveries(nowDate, delivery_plan, plan_name);
+    let lastDateRegistered = '';
+    if (deliveries.rows.length !== 0) {
+      lastDateRegistered = dayjs(deliveries.rows[(deliveries.rows.length) - 1].date).format('MM/DD/YYYY');
+      for (let i = 0; i < dates.length; i += 1) {
+        if (dayjs(lastDateRegistered).isBefore(dayjs(dates[i]).format('MM/DD/YYYY'))) {
+          greaterDates.push(dates[i]);
+        }
+      }
+    } else {
+      for (let i = 0; i < dates.length; i += 1) {
+        const parcial = dayjs(dates[i]).format('MM/DD/YYYY');
+        greaterDates.push(parcial);
+      }
+    }
+
+    for (let i = 0; i < greaterDates.length; i += 1) {
+      await connection.query(`
+        INSERT INTO deliveries ("user_id", date, "delivered") VALUES ($1, $2, $3);
+      `, [userId, greaterDates[i], false]);
+    }
+
+    const allDeliveries = await connection.query(`
+      SELECT date, delivered, id
+      FROM deliveries 
+      WHERE "user_id" = $1 ORDER BY id ASC
+    `, [userId]);
+
+    const repetedDeliveries = [];
+    for (let i = 0; i < allDeliveries.rows.length; i += 1) {
+      for (let j = i + 1; j < allDeliveries.rows.length; j += 1) {
+        if (dayjs(allDeliveries.rows[i].date).isSame(allDeliveries.rows[j].date)) {
+          repetedDeliveries.push(allDeliveries.rows[j].id);
+        }
+      }
+    }
+    // eslint-disable-next-line arrow-body-style
+    const filtered = repetedDeliveries.filter((el, i) => {
+      return repetedDeliveries.indexOf(el) === i;
+    });
+    for (let i = 0; i < filtered.length; i += 1) {
+      await connection.query(`
+        DELETE FROM deliveries WHERE id = $1
+      `, [filtered[i]]);
+    }
+    const filteredDeliveries = await connection.query(`
+      SELECT date, delivered, id
+      FROM deliveries 
+      WHERE "user_id" = $1 ORDER BY id ASC
+    `, [userId]);
+
+    const delivery_history = filteredDeliveries.rows;
+
+    res.send({
+      plan_name,
+      signature_date,
+      delivery_plan,
+      products,
+      delivery_history,
+    });
+  } catch (err) {
+    return res.sendStatus(500);
+  }
+}
 export {
   postSignature,
+  getSignature,
 };
