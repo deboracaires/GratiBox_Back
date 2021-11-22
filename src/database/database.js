11a9611f568bd1696cf25145a6e54ace30921e6b
deboracaires <deboracaires@pop-os.localdomain>
2021-11-22 20:46:35 -0300

    retirando configuracoes heroku

--- src/database/database.js
+++ src/database/database.js
@@ -2,21 +2,12 @@ import pg from 'pg';
 
 const { Pool } = pg;
 
-const databaseConfig = process.env.NODE_ENV === 'test'
-  ? {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-  }
-  : {
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  };
-
-const connection = new Pool(databaseConfig);
+const connection = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT, 10),
+  database: process.env.DB_DATABASE,
+});
 
 export default connection;
