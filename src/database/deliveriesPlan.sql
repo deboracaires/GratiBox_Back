ed53502ca34943b9e74c528da8438c732460a41f
Bruno <mguimaraesbruno@gmail.com>
2021-11-26 16:21:35 -0300

    Code review: Brunão

--- src/database/deliveriesPlan.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO delivery_plan (name) VALUES ('segunda');
-INSERT INTO delivery_plan (name) VALUES ('quarta');
-INSERT INTO delivery_plan (name) VALUES ('sexta');
-INSERT INTO delivery_plan (name) VALUES ('um');
-INSERT INTO delivery_plan (name) VALUES ('dez');
-INSERT INTO delivery_plan (name) VALUES ('vinte');
